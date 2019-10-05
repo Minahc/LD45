0fc1699da7bad950d39d9d6a1410e6ccdb79a3b9
Lene Christensen <lene.hojberg.christensen@gmail.com>
2019-10-05 15:33:57 +0200

    Initial commit

---
+++ Assets/Scripts/Colonist.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Colonist : MonoBehaviour {
+    // Start is called before the first frame update
+    public enum Type { Farmer, Builder, Woodcutter };
+	public Type type;
+	public TileBase[] tilelist;
+	public Tilemap details;
+	private Vector3 goingto = Vector3.zero*float.NaN;
+	public float speed;
+	void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+		if (type == Type.Woodcutter) {
+			if (goingto != goingto) {
+				goingto = details.GetComponent<Details>().nearestDetail(transform.position, tilelist[(int)type]);
+			}
+		}
+		transform.position += (goingto - transform.position).normalized*speed;
+		//Debug.Log(goingto);
+    }
+}
---
+++ Assets/Scripts/Details.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Details : MonoBehaviour {
+
+	public Vector2 nearestDetail (Vector2 pos, TileBase item)	{
+		Tilemap tilemap = GetComponent<Tilemap>();
+		BoundsInt bounds = tilemap.cellBounds;
+		TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+
+		for (int x = bounds.x+1; x < bounds.size.x; x++) {
+			for (int y = bounds.y+1; y < bounds.size.y; y++) {
+				TileBase tile = allTiles[x + y * bounds.size.x];
+				if (tile == item) {
+					Debug.Log(x + " " + y);
+					return tilemap.CellToWorld(new Vector3Int(x, y, 0));
+				}
+			}
+		}
+
+		return Vector2.zero*float.NaN;
+	}
+}
